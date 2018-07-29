8f6c44d2e1dc1fa7d515a77362c4e190d6d26d8f
Jose Epifanio <jose.epifanio90@gmail.com>
2018-07-29 18:41:59 -0500

    Create a custom authentication endpoint

--- hsse_api/urls.py
+++ hsse_api/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from rest_framework.authtoken import views as login_views
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', login_views.obtain_auth_token),
+    url(r'^login/$', views.Login.as_view()),
     url(r'^signin/$', views.User.as_view()),
     url(r'^users/$', views.Users.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view())
--- hsse_api/views.py
+++ hsse_api/views.py
@@ -1,10 +1,30 @@
+import pdb
 from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from . import models
 from . import serializers
+
+class Login(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        # pdb.set_trace()
+        return Response({
+            'token': token.key,
+            'user': {
+                'id': user.pk,
+                'name': user.name,
+                'email': user.email
+            }
+        })
 
 class User(APIView):
 
