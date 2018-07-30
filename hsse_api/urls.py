3876d50fe2bb861cb9f12b5c4e11c11b8d4fc616
Jose Epifanio <jose.epifanio90@gmail.com>
2018-07-29 22:38:04 -0500

    Change url regex to be a little bit more flexible

--- hsse_api/urls.py
+++ hsse_api/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.Login.as_view()),
-    url(r'^signin/$', views.User.as_view()),
-    url(r'^users/$', views.Users.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view())
+    url(r'^login/?$', views.Login.as_view()),
+    url(r'^signin/?$', views.User.as_view()),
+    url(r'^users/?$', views.Users.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/?$', views.UserDetail.as_view())
 ]
