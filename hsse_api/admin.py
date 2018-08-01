4612a9f0548ad8d2c0a19e23e4c2a57295baa728
Jose Epifanio <jose.epifanio90@gmail.com>
2018-08-01 01:41:27 -0500

    Register new models

--- hsse_api/admin.py
+++ hsse_api/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.register(models.User)
-admin.site.register(models.Site)
+admin.site.register(models.Audit_Inspection)
 admin.site.register(models.Corrective_Action)
+admin.site.register(models.Employee_Community_Activity)
+admin.site.register(models.Environmental_Indicators)
+admin.site.register(models.Monthly_Reports)
+admin.site.register(models.Report)
+admin.site.register(models.Safety_Activity)
+admin.site.register(models.Site)
+admin.site.register(models.User)
