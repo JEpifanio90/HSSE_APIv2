25a45c2f743650111cf39005aac594aa336829d9
Jose Epifanio <jose.epifanio90@gmail.com>
2018-08-06 14:46:27 -0500

    db settings

--- hsse/settings.py
+++ hsse/settings.py
@@ -85,15 +85,15 @@ WSGI_APPLICATION = 'hsse.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DATABASE', 'hsse_development'),
-        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'root'),
-        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
-    },
-    'production': dj_database_url.config(
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get('POSTGRES_DATABASE', 'hsse_development'),
+    #     'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+    #     'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'root'),
+    #     'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+    #     'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+    # },
+    'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL')
     )
 }
