3634dcc2547c43da900993b19203778e48d22337
Jose Epifanio <jose.epifanio90@gmail.com>
2018-08-06 19:40:58 -0500

    Enable cors for all hosts

--- hsse/settings.py
+++ hsse/settings.py
@@ -119,10 +119,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:4200',
-    'https://jepifanio90.github.io'
-)
+# CORS_ORIGIN_WHITELIST = (
+#     'localhost:4200',
+#     'https://jepifanio90.github.io'
+# )
 
 
 # Internationalization
@@ -138,6 +138,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
